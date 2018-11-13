26a02d602d595a100545f60eccc9525b1959439b
liu lang yi ge <1909493019@qq.com>
2018-11-13 14:05:17 +0800

    'modal-loading'

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,16 +1,53 @@
 import React, { Component } from 'react';
-import './index';
+import './index.less';
+import {Card ,Spin,Icon,Alert,Switch } from 'antd'
 
 
 class Iloading extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            loading:false
+         }
+    }
+    toggle=()=>{
+        this.setState({ loading:!this.state.loading  });
     }
     render() { 
+                const self_define_icon = (<Icon type="loading" style={{ fontSize: 24 }} spin />);
         return ( 
             <div>
-                iloading
+                <Card title="loading效果">
+                    <Spin/>
+                </Card>
+                <Card title="loading大小效果" className="spin_size_wrap">
+                    <Spin size="small"/>
+                    <Spin />
+                    <Spin size="large"/>
+                </Card>
+                <Card title="自定义图片">
+                    <Spin indicator={self_define_icon} />
+                </Card>
+                <Card title="带文字效果">
+                    <Spin tip="奔跑中..."/>
+                </Card>
+                <Card title="内容遮罩">
+                    <Spin spinning={this.state.loading} delay={500}>
+                        <Alert
+                            message="遮住了没有"
+                            description="loading状态触发时，我会被遮住！"
+                            type="info"
+                        />
+                    </Spin>
+                    <div className="control_loading_switch">
+                        <Switch
+                            checked={this.state.loading}
+                            onChange={this.toggle}
+                            checkedChildren="开"
+                            unCheckedChildren="关"
+                        />
+                    </div>
+                </Card>
             </div>
          );
     }
--- src/components/Modals/index.js
+++ src/components/Modals/index.js
@@ -1,16 +1,89 @@
 import React, { Component } from 'react';
-import './index';
+import './index.less';
+import { Card, Button, Modal } from 'antd';
 
 
 class Imodals extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            modal1:false,
+            modal2:false,
+            modal3:false,
+            modal4:false
+         }
+    }
+    handleClick=(status,type)=>{
+        this.setState({
+                [type]: status
+            });
+    }
+    handleCancle=(type)=>{
+        this.setState({ [type]: false });
+    }
+    handleOk=(type)=>{
+        this.setState({ [type]:false });
+    }
+    handleClickMes=(type)=>{
+        Modal[type]({
+            title:'信息确认',
+            content:'确定吗？'
+        })
     }
     render() { 
         return ( 
             <div>
-                imodals
+                <Card title="基础弹框">
+                    <Button type="primary" onClick={()=>this.handleClick(true,'modal1')}>Open</Button>
+                    <Button type="primary" onClick={()=>this.handleClick(true,'modal2')}>自定义页脚</Button>
+                    <Button type="primary" onClick={()=>this.handleClick(true,'modal3')}>顶部20px弹框</Button>
+                    <Button type="primary" onClick={()=>this.handleClick(true,'modal4')}>水平垂直居中</Button>
+                </Card>
+                <Modal  
+                    title="React"
+                    visible = {this.state.modal1}
+                    onCancel={()=>this.handleCancle('modal1')}
+                    onOk={()=>this.handleOk('modal1')}
+                >
+                    <p>欢迎体验此项目Open</p>
+                </Modal>
+                <Modal  
+                    title="React"
+                    visible = {this.state.modal2}
+                    okText="下一步"
+                    cancelText = "算了"
+                    onCancel={()=>this.handleCancle('modal2')}
+                    onOk={()=>this.handleOk('modal2')}
+                >
+                    <p>欢迎体验此项目自定义页脚</p>
+                </Modal>
+                <Modal  
+                    title="React"
+                    visible = {this.state.modal3}
+                    okText="确认"
+                    cancelText = "取消"
+                    onCancel={()=>this.handleCancle('modal3')}
+                    onOk={()=>this.handleOk('modal3')}
+                    style={{top:'20px'}}
+                >
+                    <p>欢迎体验此项目顶部20px</p>
+                </Modal>
+                <Modal  
+                    title="React"
+                    visible = {this.state.modal4}
+                    okText="确认"
+                    cancelText = "取消"
+                    onCancel={()=>this.handleCancle('modal4')}
+                    onOk={()=>this.handleOk('modal4')}
+                >
+                    <p>水平垂直居中</p>
+                </Modal>
+                <Card title="信息确认框"> 
+                     <Button type="primary" onClick={()=>this.handleClickMes('confirm')}>confirm</Button>   
+                     <Button type="primary" onClick={()=>this.handleClickMes('info')}>info</Button>   
+                     <Button type="primary" onClick={()=>this.handleClickMes('success')}>success</Button>   
+                     <Button type="primary" onClick={()=>this.handleClickMes('warning')}>warning</Button>   
+                </Card>
             </div>
          );
     }
